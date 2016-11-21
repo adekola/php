54e5621d17e6e93053c7f39d8d21a34972a1dfbd
Adebayo <A44079@eon-energie.net>
2016-11-21 15:45:17 +0100

    Assignment for step 7

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php 
 
+$task = [
 
-$person = [
-	'age' => 25 ,
-	'hair' => 'Black',
-	'career' => 'Products Guy'
+	'title' => 'Buy Proposal Ring',
+	'due' => 'Next Month',
+	'assigned_to' => 'Me',
+	'completed' => 'No'
 ];
 
 
-$person['name'] =  'Adekola';
-
-unset($person['age']);
 echo '<pre>';
-//die(var_dump($person)); // dumps the value for any and everything; die() makes the execution end on this very line
-var_dump($person);
+var_dump($task);
 echo '</pre>';
 
 require 'index.view.php';
